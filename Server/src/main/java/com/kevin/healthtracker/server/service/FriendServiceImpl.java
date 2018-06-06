1ab2f912f738fa0dbabaeb00fec4fe734b0ccaaa
Kevin.Tran <Kevin.Tran@veritas.com>
2018-06-06 15:37:19 +0100

    Friend service interface

--- Server/src/main/java/com/kevin/healthtracker/server/service/FriendServiceImpl.java
+++ Server/src/main/java/com/kevin/healthtracker/server/service/FriendServiceImpl.java
@@ -1,4 +1,34 @@
 package com.kevin.healthtracker.server.service;
 
-public class FriendServiceImpl {
+import java.util.List;
+
+import com.kevin.healthtracker.datamodels.Friend;
+import com.kevin.healthtracker.datamodels.FriendStatus;
+import com.kevin.healthtracker.server.service.interfaces.FriendService;
+
+public class FriendServiceImpl implements FriendService {
+    @Override
+    public Friend addFriendRelation(int user1, int user2) {
+        return null;
+    }
+
+    @Override
+    public Friend updateFriendRelation(Friend friend, FriendStatus friendStatus) {
+        return null;
+    }
+
+    @Override
+    public void deleteFriendRelation(int user1, int user2) {
+
+    }
+
+    @Override
+    public Friend getFriendRelation(int user1, int user2) {
+        return null;
+    }
+
+    @Override
+    public List<Friend> getFriendRelationList(int user1) {
+        return null;
+    }
 }
--- Server/src/main/java/com/kevin/healthtracker/server/service/interfaces/FriendService.java
+++ Server/src/main/java/com/kevin/healthtracker/server/service/interfaces/FriendService.java
@@ -1,4 +1,18 @@
 package com.kevin.healthtracker.server.service.interfaces;
 
+import java.util.List;
+
+import com.kevin.healthtracker.datamodels.Friend;
+import com.kevin.healthtracker.datamodels.FriendStatus;
+
 public interface FriendService {
+    Friend addFriendRelation(int user1, int user2);
+
+    Friend updateFriendRelation(Friend friend, FriendStatus friendStatus);
+
+    void deleteFriendRelation(int user1, int user2);
+
+    Friend getFriendRelation(int user1, int user2);
+
+    List<Friend> getFriendRelationList(int user1);
 }
