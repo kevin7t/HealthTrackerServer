78fc459dbf7f17ad7732bfe92d5e1fd51c61c964
Kevin.Tran <Kevin.Tran@veritas.com>
2018-06-08 11:24:17 +0100

    Friend service implementation

--- Server/src/main/java/com/kevin/healthtracker/server/service/FriendServiceImpl.java
+++ Server/src/main/java/com/kevin/healthtracker/server/service/FriendServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 
 import com.kevin.healthtracker.datamodels.Friend;
 import com.kevin.healthtracker.datamodels.FriendStatus;
-import com.kevin.healthtracker.datamodels.User;
 import com.kevin.healthtracker.datamodels.compositekeys.UserUserKey;
 import com.kevin.healthtracker.server.dao.FriendDaoImpl;
 import com.kevin.healthtracker.server.dao.UserDAOImpl;
@@ -24,13 +23,11 @@ public class FriendServiceImpl implements FriendService {
 
     @Override
     public Friend addFriendRelation(int user1, int user2) {
-        User firstUser = userDAO.getById(user1);
-        User secondUser = userDAO.getById(user2);
         Friend friendRelation = new Friend();
-        friendRelation.setUser1(firstUser);
-        friendRelation.setUser2(secondUser);
+        friendRelation.setUser1(userDAO.getById(user1));
+        friendRelation.setUser2(userDAO.getById(user2));
         friendRelation.setFriendStatus(FriendStatus.PENDING);
-        friendRelation.setUserActionId(firstUser.getId());
+        friendRelation.setUserActionId(user1);
         return friendDao.addFriendRelation(friendRelation);
     }
 
@@ -41,25 +38,20 @@ public class FriendServiceImpl implements FriendService {
 
     @Override
     public void deleteFriendRelation(int user1, int user2) {
-        UserUserKey key = getUserKey(user1, user2);
-        friendDao.deleteFriendRelation(friendDao.getFriendRelation(key));
+        friendDao.deleteFriendRelation(friendDao.getFriendRelation(getUserKey(user1, user2)));
     }
 
     @Override
     public Friend getFriendRelation(int user1, int user2) {
-        UserUserKey key = getUserKey(user1, user2);
-        return friendDao.getFriendRelation(key);
+        return friendDao.getFriendRelation(getUserKey(user1, user2));
     }
 
     @Override
     public List<Friend> getFriendRelationList(int user1) {
-        User user = userDAO.getById(user1);
-        return friendDao.getFriendRelationList(user);
+        return friendDao.getFriendRelationList(userDAO.getById(user1));
     }
 
     private UserUserKey getUserKey(int user1, int user2) {
-        User firstUser = userDAO.getById(user1);
-        User secondUser = userDAO.getById(user2);
-        return new UserUserKey(firstUser, secondUser);
+        return new UserUserKey(userDAO.getById(user1), userDAO.getById(user2));
     }
 }
